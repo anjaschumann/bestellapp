698428eacab888d7f58251fb701f32bfb71a5111
Anja Schumann <anja.schumann@mail.de>
2025-08-23 15:17:27 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const myBakery = [
+  {
+    name: "Stachelbeer-Baiser-Torte",
+    price: 4.2,
+    description:
+      "Lockerer Biskuitboden mit saftigen Stachelbeeren, bedeckt mit luftiger Baiserhaube.",
+  },
+  {
+    name: "Apfelkuchen",
+    price: 3.2,
+    description:
+      "Klassischer Rührteig mit frischen Apfelscheiben und Zimt-Zucker verfeinert.",
+  },
+  {
+    name: "Käsekuchen mit Streuseln",
+    price: 3.8,
+    description: "Cremiger Käsekuchen mit knuspriger Butterstreusel-Decke.",
+  },
+  {
+    name: "Pflaumenkuchen",
+    price: 3.5,
+    description:
+      "Hefeteigboden belegt mit reifen Pflaumen, leicht mit Zimt abgeschmeckt.",
+  },
+  {
+    name: "Aprikosenkuchen",
+    price: 3.6,
+    description:
+      "Saftiger Rührkuchen mit sonnengereiften Aprikosen und leichter Glasur.",
+  },
+  {
+    name: "Gedeckte Apfeltorte",
+    price: 4.0,
+    description:
+      "Feiner Mürbeteig mit Apfelfüllung und Zimt, von einer goldbraunen Teigdecke umhüllt.",
+  },
+  {
+    name: "Zimtschnecken",
+    price: 2.2,
+    description:
+      "Süßes Hefeteiggebäck mit Zimt-Zucker-Füllung und leichter Zuckerglasur.",
+  },
+  {
+    name: "Brownies",
+    price: 2.8,
+    description:
+      "Saftige Schokoladenwürfel mit leicht knuspriger Kruste und weichem Kern.",
+  },
+];
