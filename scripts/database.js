0db10503fd282fe9acbd819e0d40334c83d8906e
Anja Schumann <anja.schumann@mail.de>
2025-08-28 21:48:00 +0200

    database extended, cart button changes responsively

--- scripts/database.js
+++ scripts/database.js
@@ -8,7 +8,7 @@ const myProducts = [
       "Lockerer Biskuitboden mit saftigen Stachelbeeren, bedeckt mit luftiger Baiserhaube.",
   },
   {
-    img: "./assets/img/gedeckte-apfeltorte.png",
+    img: "./assets/img/apfelkuchen.webp",
     name: "Apfelkuchen",
     price: 3.2,
     description:
@@ -21,37 +21,51 @@ const myProducts = [
     description: "Cremiger Käsekuchen mit knuspriger Butterstreusel-Decke.",
   },
   {
-    img: "./assets/img/kaesekuchen-mit-streuseln.png",
+    img: "./assets/img/pflaumenkuchen.webp",
     name: "Pflaumenkuchen",
     price: 3.5,
     description:
       "Hefeteigboden belegt mit reifen Pflaumen, leicht mit Zimt abgeschmeckt.",
   },
   {
-    img: "./assets/img/kaesekuchen-mit-streuseln.png",
+    img: "./assets/img/aprikosenkuchen.webp",
     name: "Aprikosenkuchen",
     price: 3.6,
     description:
       "Saftiger Rührkuchen mit sonnengereiften Aprikosen und leichter Glasur.",
   },
   {
-    img: "./assets/img/kaesekuchen-mit-streuseln.png",
+    img: "./assets/img/gedeckte-apfeltorte.png",
     name: "Gedeckte Apfeltorte",
     price: 4.0,
     description:
       "Feiner Mürbeteig mit Apfelfüllung und Zimt, von einer goldbraunen Teigdecke umhüllt.",
   },
   {
-    img: "./assets/img/kaesekuchen-mit-streuseln.png",
+    img: "./assets/img/zimtschnecken.webp",
     name: "Zimtschnecken",
     price: 2.2,
     description:
       "Süßes Hefeteiggebäck mit Zimt-Zucker-Füllung und leichter Zuckerglasur.",
   },
   {
-    img: "./assets/img/kaesekuchen-mit-streuseln.png",
+    img: "./assets/img/brownies.jpg",
     name: "Brownies",
     price: 2.8,
+    description:
+      "Saftige Schokoladenwürfel mit leicht knuspriger Kruste und weichem Kern.",
+  },
+  {
+    img: "./assets/img/tartelettes au citron.webp",
+    name: "Tartelettes au citron",
+    price: 3.9,
+    description:
+      "Saftige Schokoladenwürfel mit leicht knuspriger Kruste und weichem Kern.",
+  },
+  {
+    img: "./assets/img/regenbogenkuchen.webp",
+    name: "Regenbogenkuchen",
+    price: 3.2,
     description:
       "Saftige Schokoladenwürfel mit leicht knuspriger Kruste und weichem Kern.",
   },
